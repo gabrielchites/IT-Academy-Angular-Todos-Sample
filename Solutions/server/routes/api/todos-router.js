7af11307c3e961f304cc6daffb64ffbcbd70c804
Gabriel Mello <gabrielchites@gmail.com>
2017-10-15 20:54:26 -0300

    In-memory Todos API for task TP-3

---
+++ Solutions/server/routes/api/todos-router.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+
+let todos = [
+  {text: 'remember the milk', done: false, id: 1},
+  {text: 'remember the beer', done: false, id: 2},
+  {text: 'done task', done: true, id: 3},
+];
+
+function getNewId() {
+  return _.max(_.map(todos, (todo) => todo.id)) + 1;
+}
+
+function getTodoById(id) {
+  return _.find(todos, (todo) => todo.id.toString() === id);
+}
+
+router.get('/', function (req, res) {
+  res.json(todos);
+});
+
+router.get('/:id', function (req, res) {
+  const id = req.params.id;
+
+  let todo = getTodoById(id);
+
+  if (todo) {
+    res.json(todo);
+  }
+  else {
+    res.statusCode = 404;
+    res.send(null);
+  }
+});
+
+router.post('/', (req, res) => {
+  const todo = req.body;
+  todo.id = getNewId();
+
+  todos.push(todo);
+
+  res.json(todo);
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  let todo = getTodoById(id);
+  let todoIndex = todos.indexOf(todo);
+
+  if(todoIndex < 0){
+    res.statusCode = 404;
+    res.send(null);
+    return;
+  }
+
+  todos.splice(todoIndex, 1);
+
+  res.statusCode = 200;
+  res.send(null);
+});
+
+module.exports = router;
