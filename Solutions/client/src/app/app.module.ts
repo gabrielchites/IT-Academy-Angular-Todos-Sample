1dc9069e2f0135d670a494f637a60c2688c7563d
Mello <Gabriel_de_Mello@amer.dell.com>
2017-10-18 13:13:33 -0200

    TP-4 Added todo component and simple navigation to it

--- Solutions/client/src/app/app.module.ts
+++ Solutions/client/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule }   from '@angular/router';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodosModule } from './todos/todos.module';
+
+const appRoutes = [
+  {
+    path: 'todos',
+    component: TodosComponent,
+    data: { title: 'Heroes List' }
+  },
+  // { path: '',
+  //   redirectTo: '/todos',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
